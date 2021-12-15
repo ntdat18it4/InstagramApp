16ed5becbffbf42923db3ba7f122b57487b392fd
ntdat <dat29022000@gmail.com>
2021-12-15 16:58:39 +0700

    Add Feature bottom navigation menu

--- app/src/main/java/com/example/instagramapp/MainActivity.kt
+++ app/src/main/java/com/example/instagramapp/MainActivity.kt
@@ -2,10 +2,58 @@ package com.example.instagramapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+
+    private lateinit var textMessage : TextView
+    private val onNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        when (item.itemId)
+        {
+            R.id.nav_home ->
+            {
+                textMessage.setText("Home")
+                return@OnNavigationItemSelectedListener true
+            }
+
+            R.id.nav_search ->
+            {
+                textMessage.setText("Search")
+                return@OnNavigationItemSelectedListener true
+            }
+
+            R.id.nav_add_post ->
+            {
+                textMessage.setText("Add Post")
+                return@OnNavigationItemSelectedListener true
+            }
+
+            R.id.nav_notifications ->
+            {
+                textMessage.setText("Notification")
+                return@OnNavigationItemSelectedListener true
+            }
+
+            R.id.nav_profile ->
+            {
+                textMessage.setText("Profile")
+                return@OnNavigationItemSelectedListener true
+            }
+        }
+
+        false
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navView: BottomNavigationView = findViewById(R.id.nav_view)
+        textMessage = findViewById(R.id.message)
+        navView.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
+
     }
+
 }

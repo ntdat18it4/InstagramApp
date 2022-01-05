22cd494e5a800dafe83df51c2015db7d60b264b6
ntdat <dat29022000@gmail.com>
2022-01-05 17:59:36 +0700

    Update UserDao

--- app/src/main/java/com/example/instagramapp/Database/UserDao.kt
+++ app/src/main/java/com/example/instagramapp/Database/UserDao.kt
@@ -3,17 +3,21 @@ package com.example.instagramapp.Database
 import androidx.lifecycle.LiveData
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.example.instagramapp.Model.UserModel
 
 @Dao
 interface UserDao {
 
-    @Insert
-    suspend fun insert(user: UserModel)
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun addUser(user: UserModel)
 
     //@Delete
     //suspend  fun deleteSubscriber(register: RegisterEntity):Int
+
+    @Query("SELECT * FROM User_Table ORDER BY userId ASC")
+    fun readAllUser() : LiveData<List<UserModel>>
 
     @Query("SELECT * FROM User_Table ORDER BY userId DESC")
     fun getAllUsers(): LiveData<List<UserModel>>

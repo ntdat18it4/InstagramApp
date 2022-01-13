f89a10fd0acc7c0f687e4d38aa69a602b90ea663
ntdat <dat29022000@gmail.com>
2022-01-13 10:38:46 +0700

    Update User entities

--- app/src/main/java/com/example/instagramapp/database/entities/User.kt
+++ app/src/main/java/com/example/instagramapp/database/entities/User.kt
@@ -1,14 +1,27 @@
 package com.example.instagramapp.database.entities
 
+import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity
+@Entity(tableName = "user_entity")
 data class User(
-    @PrimaryKey(autoGenerate = true)
+    @PrimaryKey(autoGenerate = false)
+    @ColumnInfo(name = "id")
     val id: Int = 0,
+
+    @ColumnInfo(name = "username")
     val username: String,
+
+    @ColumnInfo(name = "password")
     val password: String,
+
+    @ColumnInfo(name = "fullname")
     val fullName: String,
-    val email: String
+
+    @ColumnInfo(name = "email")
+    val email: String,
+
+    @ColumnInfo(name = "profilePicUrl")
+    val profilePicUrl: String?
 )
